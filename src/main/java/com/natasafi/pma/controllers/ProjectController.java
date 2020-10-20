3a61422fcee22fadfd4c9023660e41ea42d5fc4a
natasafi <natasafrag@gmail.com>
2020-10-20 20:10:02 +0100

    creates post controller for saving new project to the repo

--- src/main/java/com/natasafi/pma/controllers/ProjectController.java
+++ src/main/java/com/natasafi/pma/controllers/ProjectController.java
@@ -1,20 +1,34 @@
 package com.natasafi.pma.controllers;
 
 import com.natasafi.pma.entities.Project;
+import com.natasafi.pma.repository.ProjectRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/projects")
 public class ProjectController {
 
-    @RequestMapping("/new")
+    @Autowired
+    ProjectRepository projectRepository;
+
+    @GetMapping("/new")
     public String displayProjectForm(Model model) {
         Project aProject = new Project();
 
         model.addAttribute("project", aProject);
 
         return "newProject";
+    }
+
+    @PostMapping("/save")
+    public String createProject(Project project, Model model) {
+        projectRepository.save(project);
+
+        return "redirect:/new";
     }
 }
