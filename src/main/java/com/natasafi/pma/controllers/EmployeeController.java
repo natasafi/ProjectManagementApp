fba84154d93a48fb6895ae568e6c163c459b5423
natasafi <natasafrag@gmail.com>
2020-10-25 14:46:25 +0000

    Creates POST mapping for Employee

--- src/main/java/com/natasafi/pma/controllers/EmployeeController.java
+++ src/main/java/com/natasafi/pma/controllers/EmployeeController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -23,5 +24,12 @@ public class EmployeeController {
         model.addAttribute("employee", employee);
 
         return "newEmployee";
+    }
+
+    @PostMapping("/save")
+    public String createEmployee(Model model, Employee employee) {
+        employeeRepository.save(employee);
+
+        return "redirect:/new";
     }
 }
