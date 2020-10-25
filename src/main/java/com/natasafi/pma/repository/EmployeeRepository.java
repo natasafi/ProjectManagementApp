1040c87dc4f49d68134b3b2600ea5aae55f252e5
natasafi <natasafrag@gmail.com>
2020-10-25 17:32:51 +0000

    overrides findAll method for a list of employees

--- src/main/java/com/natasafi/pma/repository/EmployeeRepository.java
+++ src/main/java/com/natasafi/pma/repository/EmployeeRepository.java
@@ -3,5 +3,10 @@ package com.natasafi.pma.repository;
 import com.natasafi.pma.entities.Employee;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface EmployeeRepository extends CrudRepository<Employee, Long> {
+
+    @Override
+    List<Employee> findAll();
 }
