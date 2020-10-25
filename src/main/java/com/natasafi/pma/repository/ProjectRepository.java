3890de047029b5bb527716a36025374b68ad59f9
natasafi <natasafrag@gmail.com>
2020-10-25 17:16:58 +0000

    overrides findAll for a list of projects

--- src/main/java/com/natasafi/pma/repository/ProjectRepository.java
+++ src/main/java/com/natasafi/pma/repository/ProjectRepository.java
@@ -3,5 +3,10 @@ package com.natasafi.pma.repository;
 import com.natasafi.pma.entities.Project;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface ProjectRepository extends CrudRepository<Project, Long> {
+
+    @Override
+    List<Project> findAll();
 }
